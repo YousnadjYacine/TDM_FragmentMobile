6a347d746631526a5a674909ed3dee19caac0597
yacine <cy_yousnadj@esi.dz>
2016-03-14 17:05:42 +0100

    com

---
+++ testtoolbar/src/main/java/com/example/yacine/testtoolbar/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.yacine.testtoolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+     ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        String [] adapterValues= new String[] {"all","Successful","FAiled"};
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,adapterValues);
+
+        MenuItem item=menu.findItem(R.id.spinner);
+        Spinner spinner =(Spinner) MenuItemCompat.getActionView(item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),adapter.getItem(position),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id==R.id.action_favorite){
+            //Toast.makeText(this,"Favoris",Toast.LENGTH_LONG).show();
+            Intent intent= new Intent(this,Main2Activity.class);
+            startActivity(intent);
+            //return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
